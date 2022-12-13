407278b9ca07483699ab5712229d957ac1e9044a
alusoftware <113198526+alusoftware@users.noreply.github.com>
2022-12-13 18:29:18 +0100

    Minor code improvements

--- Logic/IGameMaster.cs
+++ Logic/IGameMaster.cs
@@ -1,13 +1,7 @@
 ﻿using Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Logic
 {
-    //define better, what exactly should be in the interface and what not; (what do i need in Form1.cs?)
     public interface IGameMaster
     {
         public bool IsWin();
